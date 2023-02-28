257e5706c94c788c0e9d1f3344befec698c24959
RandomGuy0114 <clarenceperez686@gmail.com>
2023-02-28 09:08:15 +0800

    registration and header

---
+++ registration.php
@@ -0,0 +1,20 @@
+<?php include 'header.php';
+?>
+<form action="codes/register.php" method="post">
+    <label for="username"> Username </label>
+    <input type="username" name="username" id="">
+    <label for="email"> Email </label>
+    <input type="email" name="email" id="">
+    <label for="password"> Password</label>
+    <input type="password" name="password" id="">
+    <label for="password"> Confirm Password</label>
+    <input type="password" name="confirm_password" id="">
+    <input type="submit" value="Register">
+    <a href="index.php"> <input type="button" value="Login"></a>
+</form>
+
+
+
+</body>
+
+</html>
