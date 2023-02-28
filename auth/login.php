ac6649b54ff37569a48d97fc24cf0635d3073773
RandomGuy0114 <clarenceperez686@gmail.com>
2023-02-28 11:15:16 +0800

    edit index form for login

---
+++ auth/login.php
@@ -0,0 +1,55 @@
+<?php
+// Initialize the session
+session_start();
+
+// Check if the user is already logged in
+if (isset($_SESSION["loggedin"])) {
+    header("location: ../dashboard.php");
+    exit;
+}
+
+// include config file
+require_once '../config/database.php';
+
+if ($_SERVER["REQUEST_METHOD"] == "POST") {
+
+    $sql = "SELECT id, email, password FROM users WHERE email = ?";
+
+    if ($stmt = mysqli_prepare($conn, $sql)) {
+
+        mysqli_stmt_bind_param($stmt, "s", $_POST["email"]);
+
+        if (mysqli_stmt_execute($stmt)) {
+
+            mysqli_stmt_store_result($stmt);
+
+            if (mysqli_stmt_num_rows($stmt) ==  1) {
+
+                mysqli_stmt_bind_result($stmt, $id, $_POST["email"], $password);
+
+                if (mysqli_stmt_fetch($stmt)) {
+                    if (password_verify($_POST["password"], $password)) {
+
+                        $_SESSION["loggedin"] = true;
+                        $_SESSION["id"] = $id;
+                        $_SESSION["email"] = $_POST["email"];
+
+                        header("location: ../dashboard.php");
+                        $successMessage = "Logged in!";
+
+                    } else {
+                        $errorMessage = "Invalid email or password!";
+                    }
+                } else {
+                    $errorMessage = "Something went wrong! Try again later.";
+                }
+                mysqli_stmt_close($stmt);
+            } else {
+                $errorMessage = "Email not found!";
+            }
+        }
+    }
+    mysqli_close($conn);
+}
+
+?>
--- index.php
+++ index.php
@@ -1,12 +1,12 @@
 <?php include('config/database.php');?>
 <?php include('header.php') ;
 ?>
-    <form action="" method="post">
+    <form action="auth/login.php" method="post">
         <label for="email"> Email </label>
         <input type="email" name="email" id="">
         <label for="password"> Password</label>
         <input type="password" name="password" id="">
-        <input type="button" value="Login">
+        <input type="submit" value="Login">
 
         <a href="registration.php"><input type="button" value="Register"></a>
     </form>
