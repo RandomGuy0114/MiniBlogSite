f3b9c98fa1479029805753ff7bd88b490a0a711d
RandomGuy0114 <clarenceperez686@gmail.com>
2023-02-28 09:07:20 +0800

    converting to php file

---
+++ index.php
@@ -0,0 +1,15 @@
+<?php include('config/database.php')?>
+<?php include('header.php') ;
+?>
+    <form action="" method="post">
+        <label for="email"> Email </label>
+        <input type="email" name="email" id="">
+        <label for="password"> Password</label>
+        <input type="password" name="password" id="">
+        <input type="button" value="Login">
+
+        <a href="registration.php"><input type="button" value="Register"></a>
+    </form>
+</body>
+
+</html>
